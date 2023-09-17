e6268b7ed1646b95d9e6e36d6271997bfdc02dea
ArtCode101 <sanayut12.service@gmail.com>
2023-09-18 00:08:19 +0700

    get account all

---
+++ script_get_accounts.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "artdb"
+})
+
+connection.connect((err) => {
+    if (err) console.log(err);
+
+    const query = "SELECT * FROM account;"
+    connection.query(query,(err,result)=>{
+        if (err) console.log(err);
+        console.log(result);
+
+        connection.end()
+    })
+})
