50d7068dca753a92f2ca679bc2c616183fae2fd8
ArtCode101 <sanayut12.service@gmail.com>
2023-09-17 23:59:27 +0700

    fix

--- script_create_account.js
+++ script_create_account.js
@@ -11,7 +11,7 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
     if (err) console.log(err);
 
-    const query = "INSERT INTO account(name,email,phone) VALUES('art2','art2@gmail.com','0912345679');"
+    const query = "INSERT INTO account(name,email,phone) VALUES('art','art@gmail.com','0912345678');"
     connection.query(query,(err,result)=>{
         if (err) console.log(err);
         console.log(result);
